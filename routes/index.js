14f9cf2050e38528a6d181455fd703e77f4d5e73
Alfonso Jimenez <aljimeruz@gmail.com>
2015-07-30 20:28:22 +0200

    Validaciones

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* Página de entrada GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 
@@ -17,7 +17,10 @@ router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+
 router.get('/author', function(req, res) {
-    res.render('author');
+    res.render('author', {errors: []});
 })
 module.exports = router;
